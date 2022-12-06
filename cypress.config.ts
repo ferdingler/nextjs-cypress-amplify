97df30b6e174b2608f16a816cfe2fb313d9ab462
Fernando Dingler <fdingler@amazon.com>
2022-12-06 14:57:48 -0800

    Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporter-config.json"
+  },
   e2e: {
     baseUrl: 'http://localhost:3000',
   },
